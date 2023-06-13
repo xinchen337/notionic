e2f0d72c9cfe46e02c5a48ae5607e232c216c955
Grass <73681883+xinchen337@users.noreply.github.com>
2023-06-13 23:37:27 +0800

    Update Home.js

--- components/Hero/Home.js
+++ components/Hero/Home.js
@@ -1,6 +1,6 @@
 import BLOG from '@/blog.config'
 import Link from 'next/link'
-import Avatar from './Avatar.js'
+import Avatar from './NotionAvatar.js'
 import Social from '../Common/Social.js'
 import { lang } from '@/lib/lang'
 import { useRouter } from 'next/router'
